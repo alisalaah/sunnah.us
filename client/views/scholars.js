9ef71cdd0619c34da87d16c8507df45b070b3800
Aric Camarata <alisalaah@gmail.com>
2015-07-15 22:25:52 +0700

    Fixed bug for rendering scholars list

--- client/views/scholars.js
+++ client/views/scholars.js
@@ -1,21 +1,14 @@
-Template.scholars.onRendered(function () {
-	this.autorun(function () {
-		var data = Meteor.scholars.findOne();
-		if (!data) return; // Not Ready yet..
-		
-		$('#scholarsTable').dataTable({
-			"order": [[0, "asc"], [3, "asc"]],
-			"paging": false
-		});
-		$('#scholarsTable_wrapper').prepend(Blaze.toHTMLWithData(Template.scholarsToolbar)).show();
-		$('#scholarsTable_filter > label').addClass('search');
-		$('#scholarsTable_filter > label > input').attr("placeholder", "Search..").focus();
-
-	});
-});
-
 Template.scholars.helpers({
 	allScholars: function () {
+		Meteor.setTimeout(function () {
+			$('#scholarsTable').dataTable({
+				"order": [[0, "asc"], [3, "asc"]],
+				"paging": false
+			});
+			$('#scholarsTable_wrapper').prepend(Blaze.toHTMLWithData(Template.scholarsToolbar)).show();
+			$('#scholarsTable_filter > label').addClass('search');
+			$('#scholarsTable_filter > label > input').attr("placeholder", "Search..").focus();
+		}, 500);
 		return Meteor.scholars.find({}, {sort: {era: 1, died: 1}});
 	}
 });
