528bef00bd73648fa4cdb7c0028f25627629c4d3
Aric Camarata <alisalaah@gmail.com>
2015-07-16 08:44:40 +0700

    Implemented Reactive Datatables plugin

--- client/views/scholars.js
+++ client/views/scholars.js
@@ -1,14 +1,55 @@
 Template.scholars.helpers({
-	allScholars: function () {
-		Meteor.setTimeout(function () {
-			$('#scholarsTable').dataTable({
-				"order": [[0, "asc"], [3, "asc"]],
-				"paging": false
+	tableData: function () {
+		return dataTableData;
+	},
+	tableOptions: {
+		id: 'scholarsTable',
+		paging: false,
+		order: [[0, "asc"], [3, "asc"]],
+		columns: [{
+				title: 'Era',
+				data: 'era',
+				className: 'hide'
+			}, {
+				title: 'Name',
+				data: 'name',
+				render: renderName,
+				className: 'alignLeft'
+			}, {
+				title: 'Born',
+				data: 'born',
+				//render: renderBorn,
+				className: 'alignCenter'
+			}, {
+				title: 'Photo',
+				data: 'died',
+				//render: renderDied,
+				className: 'alignCenter'
+			}],
+		oLanguage: {
+			sInfo: "_TOTAL_ scholars",
+			sInfoEmpty: "_TOTAL_ scholars",
+			sInfoFiltered: "found from _MAX_"
+		},
+		initComplete: function () {
+			$('#datatable').addClass('table-striped table-bordered');
+			$('.dataTables_filter > label > input').attr("placeholder", "Search").focus();
+			$('#datatable_filter').before(Blaze.toHTMLWithData(Template.scholarsToolbar));
+		},
+		fnDrawCallback: function () {
+			$.each($('.scholarName'), function () {
+				var era = $(this).data('era');
+				$(this).parent().parent()
+						.removeClass('era0 era1 era2 era3 era4 era5 era6')
+						.addClass('era' + era);
 			});
-			$('#scholarsTable_wrapper').prepend(Blaze.toHTMLWithData(Template.scholarsToolbar)).show();
-			$('#scholarsTable_filter > label').addClass('search');
-			$('#scholarsTable_filter > label > input').attr("placeholder", "Search..").focus();
-		}, 500);
-		return Meteor.scholars.find({}, {sort: {era: 1, died: 1}});
+		}
 	}
 });
+
+function dataTableData() {
+	return Meteor.scholars.find({}, {sort: {era: 1, died: 1}}).fetch(); // or .map()
+}
+function renderName(cellData, renderType, currentRow) {
+	return '<a class="scholarName" data-era="' + currentRow.era + '" href="/' + currentRow.slug + '">' + cellData + '</a>';
+}
